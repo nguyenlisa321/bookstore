61c7de4063c86bc9a5b662a98b9239d0e030c1f5
EdwardKao <edwardkao12@gmail.com>
2017-04-25 23:31:44 -0400

    changed json

--- books.php
+++ books.php
@@ -1,15 +1,15 @@
  <?php
 $return_arr = array();
 $servername = "stardock.cs.virginia.edu";
-  $username = "cs4750s17elk2fw";
-  $serverpassword ="cs4750";
-  $dbname = "cs4750s17elk2fw";
+$username = "cs4750s17elk2fw";
+$serverpassword ="cs4750";
+$dbname = "cs4750s17elk2fw";
 $db = new mysqli($servername, $username, $serverpassword, $dbname);
         if ($db->connect_error):
         die ("Could not connect to db: " . $db->connect_error);
         endif;
      
-$query = "SELECT Title, Author, Price, PicturePath, Genre, ISBN, Binding, Publisher_Price, Publisher FROM Books"; #Define query
+$query = "SELECT Title, Author, Price, Genre, ISBN, Binding, Quantity, Publisher_Price, Publisher FROM Books"; #Define query
 $result= $db->query($query) or die ("Invalid select " . $db->error);
            #Eval and store result
 while ($row = $result->fetch_assoc()) {
